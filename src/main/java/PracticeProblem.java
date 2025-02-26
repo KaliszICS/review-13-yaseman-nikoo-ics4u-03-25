8f3d89ac97612b3dadad10ab18126d4a9485b8bb
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-02-26 20:10:59 +0000

    review 13

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,5 +5,27 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static void swap(ArrayList<String> arrL, int num, int num2){
+		String word= arrL.get(num);
+		arrL.set(num, arrL.get(num2));
+		arrL.set(num2, word);
+	} 
+	public static ArrayList<Double> createArrayList(double[] arr){
+		ArrayList<Double> arrl= new ArrayList<Double>();
+		for (int i=0;i<arr.length;i++){
+			arrl.add(arr[i]);
+		}
+		return arrl;
+	}
+	public static HashMap<String, Integer> combineParallelArrays(String[] name, int[] age){
+		HashMap<String, Integer> map=  new HashMap<String, Integer>();
+		for (int i=0; i< name.length; i++){
+			map.put(name[i],age[i]);
+		}
+		return map;
+	}
+	public static void increaseAge( HashMap<String,Integer> map, String name){
+		map.put(name, map.get(name)+1);
+	}
 
 }
